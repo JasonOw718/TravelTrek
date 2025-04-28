655a9513ee6d5525080a6f4a58e7b87841a9df26
ks <kashengow@gmail.com>
2025-04-28 10:59:20 +0800

    initialize repository

---
+++ src/main/java/com/project/TravelTrek/TravelTrekApplication.java
@@ -0,0 +1,13 @@
+package com.project.TravelTrek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelTrekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelTrekApplication.class, args);
+	}
+
+}
